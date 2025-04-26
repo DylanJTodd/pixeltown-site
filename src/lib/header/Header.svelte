49f1f8fa7d6dc758ca19ab8652f551b226a770bc
Dylan Todd <dylan.todd04@gmail.com>
2025-04-26 10:44:53 -0400

    feat: Basic header outline created

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,1 +1,17 @@
-<h1>Header!</h1>
+<div style="position: relative; width: 100%;">
+    <img src="/banner.png" alt="Pixeltown banner" class="banner-image">
+
+    <img src="/logo.png" alt="Pixeltown logo" id="logo" style="position: absolute; top: 40%; left: 50%; transform: translateX(-50%);">
+
+    <nav style="position: absolute; bottom: -10%; left: 49.6%; transform: translateX(-50%); display: flex; align-items: center;">
+        <ul class="nav" style="list-style: none; display: flex; align-items: center; padding: 0; margin: 0; gap: 2rem;">
+            <li><a href="/" style="text-decoration: none;">Home</a></li>
+            <li><a href="/" style="text-decoration: none;">Join</a></li>
+        </ul>
+        <div id="logo-gap" style="flex-shrink: 0;"></div>
+        <ul class="nav" style="list-style: none; display: flex; align-items: center; padding: 0; margin: 0; gap: 2rem;">
+            <li><a href="/" style="text-decoration: none;">Vote</a></li>
+            <li><a href="/" style="text-decoration: none;">Store</a></li>
+        </ul>
+    </nav>
+</div>
