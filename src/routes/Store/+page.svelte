be83586d91838927fddfb4044d8999af2c37e87b
Dylan Todd <dylan.todd04@gmail.com>
2025-04-26 14:55:09 -0400

    feat: Added routes to other pages

---
+++ src/routes/Store/+page.svelte
@@ -0,0 +1,9 @@
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1> Testing Store Page</h1>
+</section>
---
+++ src/routes/Vote/+page.svelte
@@ -0,0 +1,9 @@
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1> Testing Vote Page</h1>
+</section>
