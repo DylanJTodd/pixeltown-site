32acc1f747ce605f4001eb5350643691ad4725d7
Dylan Todd <dylan.todd04@gmail.com>
2025-04-26 14:54:19 -0400

    Fully finished header functionality (not styling)

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,17 +1,40 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
 <div style="position: relative; width: 100%;">
-    <img src="/banner.png" alt="Pixeltown banner" class="banner-image">
+    <!-- Banner Image -->
+    <img src="/banner.png" alt="Pixeltown banner" class="banner-image" style="width: 100%;">
 
-    <img src="/logo.png" alt="Pixeltown logo" id="logo" style="position: absolute; top: 40%; left: 50%; transform: translateX(-50%);">
-
+    <!-- Navigation buttons -->
     <nav style="position: absolute; bottom: -10%; left: 49.6%; transform: translateX(-50%); display: flex; align-items: center;">
         <ul class="nav" style="list-style: none; display: flex; align-items: center; padding: 0; margin: 0; gap: 2rem;">
             <li><a href="/" style="text-decoration: none;">Home</a></li>
-            <li><a href="/" style="text-decoration: none;">Join</a></li>
+            <li><a href="/Join" style="text-decoration: none;">Join</a></li>
         </ul>
         <div id="logo-gap" style="flex-shrink: 0;"></div>
         <ul class="nav" style="list-style: none; display: flex; align-items: center; padding: 0; margin: 0; gap: 2rem;">
-            <li><a href="/" style="text-decoration: none;">Vote</a></li>
-            <li><a href="/" style="text-decoration: none;">Store</a></li>
+            <li><a href="/Vote" style="text-decoration: none;">Vote</a></li>
+            <li><a href="/Store" style="text-decoration: none;">Store</a></li>
         </ul>
     </nav>
+    <!-- Logo Image -->
+    <a href="/" style="position: absolute; top: 40%; left: 50%; transform: translateX(-50%);">
+        <img src="/logo.png" alt="Pixeltown logo" id="logo">
+    </a>
+
+    <!-- Discord and IP Banner Links-->
+    <a class="banner-links" href="/" style="left: 1%; display: flex; flex-direction: column; align-items: flex-start; gap: 0.5rem;"> 
+        <div style="display: flex; align-items: center; gap: 0.5rem; line-height: 0.1;">
+            <i class="fa-solid fa-copy"></i>
+            <p style="margin: 0;">Join The Discord</p>
+        </div>
+        <p style="margin: 0; font-size: 80%; font-weight: 400; line-height: 0.1;">Click to copy</p>
+    </a>
+
+    <a class="banner-links" href="/" style="right: 1%; display: flex; flex-direction: column; align-items: flex-start; gap: 0.5rem;"> 
+        <div style="display: flex; align-items: center; gap: 0.5rem; line-height: 0.1;">
+            <i class="fa-solid fa-copy"></i>
+            <p style="margin: 0;">Copy Server IP</p>
+        </div>
+        <p style="margin: 0; font-size: 80%; font-weight: 400; line-height: 0.1;">Click to copy</p>
+    </a>
 </div>
