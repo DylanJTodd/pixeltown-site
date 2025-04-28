28e8b7afd14f5613cae45d0892ba63315e5cf21d
Dylan Todd <dylan.todd04@gmail.com>
2025-04-27 20:15:17 -0400

    feat: Added vote page

--- src/routes/Vote/+page.svelte
+++ src/routes/Vote/+page.svelte
@@ -1,9 +1,28 @@
-
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Vote for PixelTown</title>
+	<meta name="description" content="Vote for the PixelTown server and earn rewards!" />
 </svelte:head>
 
-<section>
-	<h1> Testing Vote Page</h1>
-</section>
+<div class="vote-container">
+	<div class="vote-content">
+		<h1>Vote for PixelTown</h1>
+		<p>
+			Vote for our server using each of the links below to receive rewards in-game! 
+			You can vote every 24 hours on each vote link, and multiple times on some. 
+			Every vote will give you a vote key, and chances at awesome rewards!
+		</p>
+	</div>
+	<img src="/Ghimmighoul.png" alt="Vote Placeholder" class="vote-image" />
+</div>
+
+<hr class="divider" />
+
+<div class="vote-links">
+	<a href="#" class="vote-link">Placeholder</a>
+	<a href="#" class="vote-link">Placeholder</a>
+	<a href="#" class="vote-link">Placeholder</a>
+	<a href="#" class="vote-link">Placeholder</a>
+	<a href="#" class="vote-link">Placeholder</a>
+	<a href="#" class="vote-link">Placeholder</a>
+	<a href="#" class="vote-link">Placeholder</a>
+</div>
